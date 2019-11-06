16e424218a76b0abc0c7d1f4aeefe52bd8f759ce
lijieniu <niulijie19890522@163.com>
2019-11-06 21:18:44 +0800

    form 完善

--- src/Form.vue
+++ src/Form.vue
@@ -1,7 +1,32 @@
 <template>
-    <el-form :model="formData">
-        <el-form-item>
-            <el-input></el-input>
+    <el-form class="sr-form" :model="formData">
+        <el-form-item 
+            class="sr-form-item" 
+            v-for="(form, index) in forms" 
+            :key="index" 
+            :label="form.label">
+            <el-input 
+                v-if="['input', 'textarea', 'button', 'checkbox', 'number', 'password', 'radio'].includes(form.type)"
+                :size="form.size"
+                :disabled="form.disabled"
+                :type="form.type"
+                :placeholder="form.placeholder"
+                @input="handleInput(form, $event)"
+                autosize></el-input>
+            <el-date-picker
+                v-if="['datetime', 'date'].includes(form.type)"
+                :type="form.type"
+                :placeholder="form.placeholder"></el-date-picker>
+            <el-select
+                v-if="form.type === 'select'"
+                :type="form.type"
+                :placeholder="form.placeholder">
+                <el-option
+                    v-for="option in form.options"
+                    :key="option.value"
+                    :label="option.label"
+                    :value="option.value"></el-option>    
+            </el-select>
         </el-form-item>
     </el-form>
 </template>
@@ -15,11 +40,34 @@ export default {
         }
     },
     data() {
+        console.log(this.form);
         return {
             forms: this.form,
             formData: {}
         }
+    },
+    methods: {
+        handleInput(form, event) {
+            console.log(event);
+            form.input(222);
+        }
     }
 }
 </script>
+<style lang="scss">
+.sr-form {
+    display: flex;
+    justify-content: flex-start;
+    margin-top: 30px;
+    flex-wrap: wrap;
+    width: 80%;
+    margin: 30px auto;
+    &-item {
+        min-width: 250px;
+        display: flex;
+        margin-right: 40px;
+    }
+}
+</style>
+
 
--- src/plugins/elementui.js
+++ src/plugins/elementui.js
@@ -1,9 +1,15 @@
 import { 
     Form,
     FormItem,
-    Input
+    Input,
+    DatePicker,
+    Select,
+    Option
 } from 'element-ui';
 
 Vue.use(Form);
 Vue.use(FormItem);
 Vue.use(Input);
+Vue.use(DatePicker);
+Vue.use(Select);
+Vue.use(Option);
